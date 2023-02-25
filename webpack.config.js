eb7935494a4d528e802e65a4303d77615c19ba8c
rabie-khlifi <rabie.rabi@gmail.com>
2023-02-25 09:56:42 +0100

    changes script to be added bottom of file

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,25 @@ const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: {
-     bundle: path.resolve(__dirname,  "src/js/index.ts"),
+    bundle: path.resolve(__dirname, "src/js/index.ts"),
   },
   mode: "development",
   devServer: {
     watchFiles: ["src/**/*"],
   },
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin(),
+      new TerserPlugin(),
+    ]
+  },
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.tsx?$/,
         use: "ts-loader",
         exclude: /node_modules/,
@@ -24,7 +31,7 @@ module.exports = {
         use: [
           // could replace the next line with "style-loader" here for inline css
           MiniCssExtractPlugin.loader,
-          "css-loader", 
+          "css-loader",
           "postcss-loader",
           'sass-loader',
         ],
@@ -39,10 +46,14 @@ module.exports = {
       title: 'Webpack App',
       filename: 'index.html',
       template: 'src/index.html',
+      inject: 'body' // adds script to bottom of body
       // chunks: [], // use this to apply js files to certain files
 
     }),
-    new MiniCssExtractPlugin({ filename: "css/[name].[hash].css" }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[hash].css"
+    }),
+
   ],
   output: {
     filename: "js/[name].[contenthash].js",
